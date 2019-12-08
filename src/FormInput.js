f73715d2504cac46d62e7cff8aba6ce1627a9a83
Trystan Hendricks <dolkhound@gmx.com>
2019-12-07 23:02:56 -0500

    added change event and initialState

--- src/FormInput.js
+++ src/FormInput.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
 
 class FormInput extends Component {
+    constructor(props) {
+        super(props)
 
+        this.initState = {
+            album: '',
+            name: '',
+        }
+
+        this.state = this.initState;
+    }
+    
+    handleChange = event => {
+        const { name, value } = event.target;
+    
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    render() {
+        const { name, album } = this.state;
+
+        return(
+            <form>
+                <label>Album Name</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={this.handleChange} />
+                <label>Band Name</label>
+                <input 
+                    type="text"
+                    value={album}
+                    onChange={this.handleChange} />
+            </form>
+        );
+    }
 }
 
 export default FormInput;
